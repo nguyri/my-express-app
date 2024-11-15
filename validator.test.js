27170506b0b3f4264dc1671cfd1bb08940778dcf
Richard Nguyen <11547131+nguyri@users.noreply.github.com>
2024-11-15 15:58:25 -0700

    Added at least 1 unit test to each validator function

--- validator.test.js
+++ validator.test.js
@@ -55,3 +55,78 @@ test ('suitGroups check', () => {
     console.log(result);
     expect(areNestedArraysEqual(expected, result)).toBe(true);
 });
+
+test ('hasMeld check', () => {
+    const expected = [true, true, true, true];
+    const tiles = E.handTiles('b1b2b3c1c2c3m1m2m3dgdgdgdrdr');
+    const suitGroups = E.suitGroups(tiles);
+    const meldResults = suitGroups.map((group) => (E.hasMeld(group)));
+    console.log(meldResults);
+    expect(areNestedArraysEqual(expected, meldResults)).toBe(true);
+})
+
+test ('hasMeldAndPair check', () => {
+    expect(false).toBe(true);
+    //todo
+})
+
+test ('validMelds check', () => {
+    expect(false).toBe(true);
+    //todo
+})
+
+test ('validMeld check', () => {
+    expect(false).toBe(true);
+    //todo
+})
+
+test ('isStraight check', () => {
+    const input = ['b1','b2','b3']
+    expect(E.isStraight(input)).toBe(true);
+})
+
+test ('isTriple check', () => {
+    const input = ['b1','b1','b1']
+    expect(E.isTriple(input)).toBe(true);
+})
+
+test ('inputIsTiles check', () => {
+    const input = ['b1','b2','b3'];
+    expect(E.inputIsTiles(input)).toBe(true);
+})
+
+test ('getNumbers check', () => {
+    const input = [1,2,3]
+    const subTile = ['b1','b2','b3'];
+    expect(areNestedArraysEqual(E.getNumbers(subTile),input)).toBe(true);
+})
+
+test ('getSuits check', () => {
+    const input = [b,c]
+    const subTile = [['b1','b2','b3'],['c1','c2','c3']];
+    expect(areNestedArraysEqual(E.getSuits(subTile),input)).toBe(true);
+})
+
+test ('validSuit check', () => {
+    expect(E.validSuit('b1')).toBe(true);
+})
+
+test ('validPair check', () => {
+    const input = ['b1','b1']
+    expect(E.validPair(input)).toBe(true);
+})
+
+test ('riichi check', () => {
+    const str = 'b1b2b3c1c2c3m1m2m3dgdgdgdrdr';
+    expect(E.riichi(str)).toBe(true);
+})
+
+test ('tileOrder check', () => {
+    expect(E.tileOrder('b1','b2') === -1).toBe(true);
+})
+
+test ('handTiles check', () =>  {
+    const str = 'b1b2b3c1c2c3m1m2m3dgdgdgdrdr';
+    const expected = ['b1','b2','b3','c1','c2','c3','m1','m2','m3','dg','dg','dg','dr','dr'];
+    expect(areNestedArraysEqual(E.handTiles(str),expected)).toBe(true);
+})
